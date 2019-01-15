e70c4c1b5848a9881367effee31085d9e1e8a703
Ivan López <ivan.lopez@studentpartner.com>
2019-01-14 22:43:29 -0600

    Heroku6

--- AguaPotable/settings.py
+++ AguaPotable/settings.py
@@ -97,12 +97,12 @@ WSGI_APPLICATION = 'AguaPotable.wsgi.application'
 #         'PORT': '5432',
 #     }
 # }
-DATABASES = {}
+
 
 #Update database configuration with $DATABASE_URL.
 import dj_database_url
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
+DATABASES = {}
+DATABASES['default'] = dj_database_url.config()
 
 
 
