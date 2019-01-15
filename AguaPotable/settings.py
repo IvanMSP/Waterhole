6c20f6b7a43530fd8689f9cd7fdb38d5d1487f21
Ivan López <ivan.lopez@studentpartner.com>
2019-01-14 22:07:26 -0600

    Heroku6

--- AguaPotable/settings.py
+++ AguaPotable/settings.py
@@ -97,9 +97,18 @@ WSGI_APPLICATION = 'AguaPotable.wsgi.application'
 #         'PORT': '5432',
 #     }
 # }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+#Update database configuration with $DATABASE_URL.
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
+
 
 
 # Password validation
