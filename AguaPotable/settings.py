aa67d27d50be1ce30467a21d569146c83e2616ae
Ivan López <ivan.lopez@studentpartner.com>
2019-01-14 23:16:34 -0600

    Heroku6

--- AguaPotable/settings.py
+++ AguaPotable/settings.py
@@ -93,8 +93,18 @@ DATABASES = {
 }
 
 #Update database configuration with $DATABASE_URL.
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+#Update database configuration with $DATABASE_URL.
 import dj_database_url
-DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
 
 
 
